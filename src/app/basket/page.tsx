2f1ab726c9e8d57b06623e8a9820b42bbeecc881
Luke Fryer <luke@mcconversions.uk>
2025-05-26 09:25:33 +0000

    I want you to send the pay with PayPal and continue shopping buttons while on top of each other. Continue shopping being on the boat.

--- src/app/basket/page.tsx
+++ src/app/basket/page.tsx
@@ -116,22 +116,25 @@ export default function BasketPage() {
               <p className="text-xs text-muted-foreground mt-1">(Excl. VAT & Delivery)</p>
             </div>
 
-            <div className="flex flex-col sm:flex-row gap-3 w-full sm:justify-end">
-              <Button asChild variant="outline" size="lg" className="w-full sm:w-auto">
-                <Link href="/">
-                  <ArrowRight className="mr-2 h-5 w-5 transform rotate-180" /> Continue Shopping
-                </Link>
-              </Button>
-              <Button 
-                asChild 
-                size="lg" 
-                className="w-full sm:w-auto bg-blue-600 hover:bg-blue-700 text-white font-semibold"
+            <div className="flex flex-col gap-3 items-center sm:items-end">
+              {/* Pay with PayPal Button (Top) */}
+              <Button
+                asChild
+                size="lg"
+                className="w-full max-w-sm sm:w-auto bg-blue-600 hover:bg-blue-700 text-white font-semibold"
               >
                 <Link href="/checkout">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-2" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M8.326 4.111c.404-.196.606.096.48.492l-2.05 6.993c-.11.375.068.66.45.66h2.727c2.56 0 4.259-1.215 4.527-3.708.215-1.96-.859-3.016-2.739-3.016h-2.063c-.233 0-.39-.114-.31-.325l.836-2.096c.08-.21.242-.35.46-.35h2.563c.382 0 .58-.275.47-.643L11.251.53C11.141.176 10.94 0 10.557 0H4.493c-.383 0-.581.276-.471.643l1.746 4.389c.11.276-.068.562-.45.562H3.165c-2.31 0-3.621 1.5-3.165 4.027.382 2.13 1.968 3.334 4.027 3.334h1.478c.55 0 .836.383.709.909l-1.715 5.503c-.128.41.053.709.442.709h4.027l.096-.3c.128-.41.347-.677.693-.677h.958c2.822 0 5.138-1.58 5.626-4.6.382-2.406-.766-3.85-2.806-3.85h-2.096c-.347 0-.548-.259-.45-.612l1.698-5.765z"/>
                   </svg>
                   Pay with PayPal
+                </Link>
+              </Button>
+
+              {/* Continue Shopping Button (Bottom) */}
+              <Button asChild variant="outline" size="lg" className="w-full max-w-sm sm:w-auto">
+                <Link href="/">
+                  <ArrowRight className="mr-2 h-5 w-5 transform rotate-180" /> Continue Shopping
                 </Link>
               </Button>
             </div>
@@ -141,3 +144,4 @@ export default function BasketPage() {
     </div>
   );
 }
+

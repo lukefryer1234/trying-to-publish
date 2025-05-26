22bc060b8764e7cd87fe5958df464e58c5353a7e
Luke Fryer <luke@mcconversions.uk>
2025-05-26 10:20:27 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SelectTrigger id="beamSize" className="w-full bg-input/50 text-center">
    
    with the text content:
    
        8 inch x 8 inch
    
    Relevant files:
    
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/src_components_d200251b._.js
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    When choosing a selection in this box or when you open the drop-down box, there is a little tick to the left hand side of the choice which doesn't need to be there when people select the choice. The choice shows up on the page anyway, we don't need the tick.

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -119,16 +119,12 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 px-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
     )}
     {...props}
   >
-    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-      <SelectPrimitive.ItemIndicator>
-        <Check className="h-4 w-4" />
-      </SelectPrimitive.ItemIndicator>
-    </span>
+    {/* Removed the span that contained the SelectPrimitive.ItemIndicator and Check icon */}
     <div className="flex-1 text-center">
       <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
     </div>

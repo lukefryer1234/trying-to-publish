fd7cba699a520b266e34c0b556b586297bb02a8c
Luke Fryer <luke@mcconversions.uk>
2025-05-26 10:18:43 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SelectTrigger id="beamSize" className="w-full bg-input/50 text-center">
    
    with the text content:
    
        6 inch x 6 inch
    
    Relevant files:
    
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/src_components_d200251b._.js
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    Selections within the drop-down box are still not centered. I want them centered.

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -119,7 +119,7 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm text-center outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className
     )}
     {...props}
@@ -129,8 +129,9 @@ const SelectItem = React.forwardRef<
         <Check className="h-4 w-4" />
       </SelectPrimitive.ItemIndicator>
     </span>
-
-    <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+    <div className="flex-1 text-center">
+      <SelectPrimitive.ItemText>{children}</SelectPrimitive.ItemText>
+    </div>
   </SelectPrimitive.Item>
 ))
 SelectItem.displayName = SelectPrimitive.Item.displayName

285ebb0033a86cc8cc64ae30b2b9ba2079efb4bd
Luke Fryer <luke@mcconversions.uk>
2025-05-26 10:09:43 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div role="radiogroup" aria-required=false dir="ltr" className="flex justify-center flex-wrap gap-4 pt-1" tabIndex=0 style={"outline":"none"}>
    
    with the text content:
    
        CurvedStraight
    
    Relevant files:
    
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_9649253b._.js
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_9649253b._.js
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_9649253b._.js
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_9649253b._.js
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_9649253b._.js
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_9649253b._.js
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_9649253b._.js
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_9649253b._.js
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_1a6d8a38._.js
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/src_components_d200251b._.js
        - https://6000-firebase-studio-1748246743230.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    These need to be squares, not rectangles

--- src/components/ProductConfigurationForm.tsx
+++ src/components/ProductConfigurationForm.tsx
@@ -272,11 +272,11 @@ export function ProductConfigurationForm({ product }: ProductConfigurationFormPr
                       <Label 
                         key={val.value} 
                         htmlFor={`${option.id}-${val.value}`} 
-                        className={`flex flex-col items-center space-y-2 p-4 border-2 rounded-lg hover:border-primary/70 cursor-pointer transition-all w-48 h-auto ${currentValue === val.value ? 'border-primary ring-2 ring-primary/50' : 'border-border'}`}
+                        className={`flex flex-col items-center justify-center space-y-2 p-3 border-2 rounded-lg hover:border-primary/70 cursor-pointer transition-all w-40 h-40 ${currentValue === val.value ? 'border-primary ring-2 ring-primary/50' : 'border-border'}`}
                       >
                         <RadioGroupItem value={val.value} id={`${option.id}-${val.value}`} className="sr-only" />
                         {val.imageUrl && (
-                          <div className="relative w-42 h-30 rounded overflow-hidden mb-1">
+                          <div className="relative w-28 h-28 rounded overflow-hidden mb-1">
                             <Image 
                               src={val.imageUrl} 
                               alt={val.label} 
@@ -613,7 +613,7 @@ export function ProductConfigurationForm({ product }: ProductConfigurationFormPr
                     >
                       <RadioGroupItem value={val.value} id={`${option.id}-${val.value}`} className="sr-only" />
                       {val.imageUrl && (
-                        <div className="relative w-36 h-24 rounded overflow-hidden">
+                        <div className="relative w-24 h-24 rounded overflow-hidden">
                           <Image 
                             src={val.imageUrl} 
                             alt={val.label} 
